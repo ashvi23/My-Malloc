5b611f717f88093ad53045fb5dcf65a4d87fa465
Esther Martens <esm108@scarletmail.rutgers.edu>
2019-03-01 15:04:19 -0500

    FINALLY got splitBin and setInUse working
    still a lot of debugging outputs in it tho, not clean

--- splitBin.c
+++ splitBin.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 void splitBin(unsigned int currInt, unsigned char* hiMeta, unsigned char* loMeta);
 void printBin(unsigned char hi, unsigned char lo);
+void printSingleBin(unsigned char byte);
 void printBinInt(unsigned int val);
-void setInUse(unsigned int inUse, unsigned char* hiMeta);
+void setInUse(int inUse, unsigned char* hiMeta);
 int main(int argc, char** argv){
-  unsigned int input=2;
+  unsigned int input=93;
   unsigned char hi='0';
   unsigned char lo='0';
 printf("begin: \n");
@@ -13,22 +14,38 @@ printBin(hi, lo);
   unsigned char* ptrhi=&hi;
   unsigned char* ptrlo=&lo;
   splitBin(input, ptrhi, ptrlo);
-  setInUse(1, ptrhi);
+printf("high: ");
+printSingleBin(hi);
+printf("low: ");
+printSingleBin(lo);
+  setInUse(0, ptrhi);
+  printf("calling printBin: \n");
   printBin(hi, lo);
   return 0;
 }
-void setInUse(unsigned int inUse, unsigned char* hiMeta){
+/*sets the in use bit , takes in an integer (functioning as a boolean), to tell it whether to assign the val to be either 1 or 0*/
+void setInUse(int inUse, unsigned char* hiMeta){
     unsigned int mask=1<<7;
     unsigned int currBit=0;
   if(inUse==0){
-    *hiMeta|=(*hiMeta & ~mask) | ((currBit << 7) & mask);
+    *hiMeta=(*hiMeta & ~mask) | ((currBit) & mask);
   } else if(inUse==1){
         currBit=1<<7;
-        *hiMeta|=(*hiMeta & ~mask) | ((currBit << 7) & mask);
+	printf("currBit: ");
+	printBinInt(currBit);
+	printf("mask: ");
+	printBinInt(mask);
+	printf("*hiMeta: ");
+	printSingleBin(*hiMeta);
+	unsigned int combo=(*hiMeta & ~mask);
+	printf("*hiMeta & ~mask: ");
+	printBinInt(combo);
+        *hiMeta=(*hiMeta & ~mask) | ((currBit) & mask);
   }
     return;
 }
 /*
+it effectively works like a function to set size metadata
 use after int arithmetic to save new sum to metadata chars
 send in an int, this extracts the bits from it and directly inserts them into the meta data chars using the given high and low byte pointers to the metadata -> int holds 615 (equiv.: 0000001001100111): *hiMeta:00000010 	*loMeta: 01100111
 (as of right now it deletes the value in the 'in use' bit when assigning the new val to the chars)
@@ -41,16 +58,18 @@ printBinInt(currInt);
   unsigned int mask=0;
   unsigned int currBit=0;
   for(int i=11; i>=0; i--){
+    printf("i: %d \n", i);
     mask=0;
     currBit=currInt>>i;
     if(i<=7){
         mask=1<<(i);
         *loMeta=(*loMeta & ~mask) | ((currBit << i) & mask);
     } else{
-      mask=1<<(i-7);
-      *hiMeta=(*hiMeta & ~mask) | ((currBit << i) & mask);
+      mask=1<<(i-8);
+      *hiMeta=(*hiMeta & ~mask) | ((currBit << (i-8)) & mask);
     }
-
+    		printf("currBit ");
+		printBinInt(currBit);
 		printf("mask: ");
 		printBinInt(mask);
 		printf("at %d: ", i);
@@ -64,7 +83,7 @@ void printBin(unsigned char hi, unsigned char lo){
 	int currBit=0;
 	for(int i=15; i>=0; i--){
 		if(i>7){
-			shift=(i-7);
+			shift=(i-8);
 			currBit=hi>>shift;
 		} else{
 			shift=i;
@@ -92,5 +111,20 @@ void printBinInt(unsigned int val){
 			}
 	}
 		printf("\n");
+	return;
+}
+void printSingleBin(unsigned char byte){
+	int shift=0;
+	int currBit=0;
+	for(int i=7; i>=0; i--){
+		shift=i;
+		currBit=byte>>shift;
+		if(currBit & 1){
+			printf("1");
+			}else{
+				printf("0");
+				}
+	}
+	printf("\n");
 	return;
 }
