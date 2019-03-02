19c2ae23b1406c7ab4758a5264cf0aa8946e6aea
Esther Martens <esm108@scarletmail.rutgers.edu>
2019-03-02 10:23:48 -0500

    stuck more comments in

--- mallocTraverse.c
+++ mallocTraverse.c
@@ -1,13 +1,13 @@
 unsigned char* mallocTraverse(unsigned char* curr, int dataSize){
     *curr=myMem[2];
     while(curr>=lowerBound && curr<=upperbound){//lowerBound & upperBound will be global vars
-      int currMeta=bin2int(*curr, *(curr+1));
-      int inUse=getbit(currMeta, 1);//hopefully this works
-      char hiBits=getsizeBits(*curr);//blocking out in use bit
+      int currMeta=bin2int(*curr, *(curr+1));//converting chars to an int to send to getbit
+      int inUse=getbit(currMeta, 1);//getting the in use bit
+      char hiBits=getsizeBits(*curr);//blocking out in use bit to get size of block
       int currBlockSize=bin2int(hiBits, *(curr+1));//getting block size of current block
-      if(inUse==0 && currBlockSize>=dataSize){
-          curr=splitBlock(curr, currBlockSize, dataSize);
-          return curr+2;
+      if(inUse==0 && currBlockSize>=dataSize){//if there's enough space to return to the user
+          curr=splitBlock(curr, currBlockSize, dataSize);//prepare it to return
+          return curr+2;//return pointer to actual data block, not trying to overwrite metadata
       } else{
           curr=(curr+currBlockSize);
       }
