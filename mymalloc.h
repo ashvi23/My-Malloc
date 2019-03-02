c47a03807b3bae8e1990b085f545c1522db4c8a2
Esther Martens <esm108@scarletmail.rutgers.edu>
2019-03-02 10:44:13 -0500

    possibly finished writing header file?

--- mymalloc.h
+++ mymalloc.h
@@ -1,2 +1,7 @@
-#define malloc mymalloc
-  void* mymalloc(int, __FILE__, __LINE__)
+#include <stdio.h>
+#include <stdlib.h>
+#define malloc(x) mymalloc(x,__FILE__,__LINE__)
+#define free(x) myfree(x,__FILE__,__LINE__)
+    char myMem[4096];
+    void* mymalloc(int, __FILE__, __LINE__);
+    void myfree(void*, __FILE__, __LINE__);
