ea2fc3ab1877804508e934d2b6eb8639ad220bad
esmartens <47702686+esmartens@users.noreply.github.com>
2019-03-09 13:40:17 -0500

    macro for key added

--- mymalloc.h
+++ mymalloc.h
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #define malloc(x) mymalloc(x,__FILE__,__LINE__)
 #define free(x) myfree(x,__FILE__,__LINE__)
+#define KEY 11474
 
     char myMem[4096];
     void* mymalloc(int, char *filename, int linenum);
